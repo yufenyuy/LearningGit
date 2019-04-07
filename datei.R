a72ecb342f01fdae41be134c141b896a792d01f6
yufenyuy <emmanuel.en23@gmail.com>
2019-04-07 18:46:06 +0200

    have ad md, txt and py files

--- datei.R
+++ datei.R
@@ -6,3 +6,5 @@ iris_dataset <- iris
 names(iris_dataset)
 head(iris_dataset)
 str(iris_dataset)
+ggplot(iris_dataset, mapping = aes(x = Sepal.Length, Sepal.Width, color = Petal.Length)) + 
+  geom_point()
